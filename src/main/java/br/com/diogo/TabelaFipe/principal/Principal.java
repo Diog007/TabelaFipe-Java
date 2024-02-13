f0e7a6236ff7cf064a6e1fa404b876b7d587eae1
Diog007 <diogo.mz123@gmail.com>
2024-02-13 12:00:33 -0300

    Created method of searching for all vehicle brands

---
+++ src/main/java/br/com/diogo/TabelaFipe/principal/Principal.java
@@ -0,0 +1,27 @@
+package br.com.diogo.TabelaFipe.principal;
+
+import br.com.diogo.TabelaFipe.model.MarcasCarros;
+import br.com.diogo.TabelaFipe.services.ConsumoApi;
+import br.com.diogo.TabelaFipe.services.ConverterDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverterDados conversor = new ConverterDados();
+
+    private final String ENDERECO = "https://parallelum.com.br/fipe/api/v1/";
+    private final String MARCA = "/marcas";
+    public void exibeMenu(){
+        System.out.println("***** OPÇÔES *****\n" + "Carros\n" + "Motos\n" + "Caminhoes\n");
+        System.out.println("Digite uma das opcoes para consultar valores:");
+        String marcaDoCarro = leitura.nextLine();
+        String endereco = marcaDoCarro;
+        var json = consumo.obterDados("https://parallelum.com.br/fipe/api/v1/" + marcaDoCarro + MARCA);
+        System.out.println(json);
+
+    }
+}
